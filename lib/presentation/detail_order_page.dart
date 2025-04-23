2f9bb3c94d7e1bce70b770dbc178bb466897e11b
nrismawardah <nrismawardah@gmail.com>
2025-04-23 23:22:37 +0700

    Membuat stateless widget untuk detail order page

---
+++ lib/presentation/detail_order_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  const DetailOrderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
