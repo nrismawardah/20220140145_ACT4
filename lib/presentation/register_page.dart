113ce224340accaf8d6c7a141b71026dc2629cc4
nrismawardah <nrismawardah@gmail.com>
2025-04-23 22:57:44 +0700

    Membuat tampilan untuk register page

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -18,6 +18,46 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Register'),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '\login');
+                },
+                child: Text('Sudah punya akun? Login sekarang!'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
